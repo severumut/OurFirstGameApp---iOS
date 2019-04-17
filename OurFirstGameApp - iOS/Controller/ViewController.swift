231de205c19c29b2513fc1c57525b135acc8cd44
Umut Sever <severumut@yahoo.com>
2019-04-17 17:31:11 +0300

    Initial Commit

---
+++ OurFirstGameApp - iOS/Controller/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OurFirstGameApp - iOS
+//
+//  Created by Umut Sever on 17.04.2019.
+//  Copyright © 2019 Umut Sever. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ OurFirstGameApp - iOS/Model/StoryLine.swift
@@ -0,0 +1,9 @@
+//
+//  StoryLine.swift
+//  OurFirstGameApp - iOS
+//
+//  Created by Umut Sever on 17.04.2019.
+//  Copyright © 2019 Umut Sever. All rights reserved.
+//
+
+import Foundation
